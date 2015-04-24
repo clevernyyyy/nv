36c2a9a0fb3944f666ed37276778b3d97373583b
clevernyyyy <aschaal1263@gmail.com>
2015-04-23 20:48:23 -0500

    action for the login click to get django working with it

--- static/envy/app/components/login-box.js
+++ static/envy/app/components/login-box.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    checkLogin: function() {
+      var bool = false;
+
+      //check here
+
+      if (bool ) {
+        alert("Worked");
+      } else  {
+        alert("Didn't work");
+      }
+    }
+  }
 });
